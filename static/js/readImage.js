77d71591014cd2f77724e5e1d70658ed7e11281d
Madison May <madison@indico.io>
2014-11-28 15:54:28 -0500

    RGBA to greyscale

--- static/js/readImage.js
+++ static/js/readImage.js
@@ -5,10 +5,10 @@ ctx = c.getContext('2d');
 
 pixels = ctx.getImageData(0,0, c.width, c.height).data;
 n_pixels = c.width*c.height;
-for (p=0;p<n_pixels;p++){
-      var x = p%width;
-        var y = Math.floor(p/width);
-          Array[x][y] = [pixels[p*4],pixels[p*4+1],pixels[p*4+2]]
+for (p=0;p<n_pixels;p++) {
+    var x = p%width;
+    var y = Math.floor(p/width);
+    Array[x][y] = [pixels[p*4],pixels[p*4+1],pixels[p*4+2]]
 }
 
 });
